1ef2ce23b8a30c45e38055866b14b91ab9bbdac7
Aubert-co <blacket021@gmail.com>
2024-07-04 23:52:02 -0300

    updated

--- app/backend/routes/adminstration_news.js
+++ app/backend/routes/adminstration_news.js
@@ -3,48 +3,33 @@ const authMidlleware = require('../Middleware/auth')
 const {News,Elements} = require('../models/index')
 const fileUpload = require('express-fileupload')
 const {createPathImg,existImg,savesImg} =require('../helpers/saveFiles')
-
+const fs = require('fs').promises
 const file_element = (order)=>`file-element${order}`
 
 route
 .use(authMidlleware)
-
-
-
-.post('/news/create',fileUpload({createParentPath:true}),async(req,res)=>{
+.use( fileUpload({createParentPath:true}) )
+.post('/news/create',async(req,res)=>{
     const {user_id} = req.user
-
-   
     const {title,resume,elements} = req.body
     let keys
     if(!title) return res.status(400).send({message:''})
         
-  
-  
     if(req.files)keys  = Object.keys(req.files)
     if(!keys.includes('file-main'))return res.status(400).send({message:''})
 
-  
     try{
         
-        let {imgPath} = createPathImg(req.files['file-main'])
+        const {imgPath} = createPathImg(req.files['file-main'])
         const news = await News.create({
             imgPath,title,resume,creator:user_id
         })
         
-        
-       
-    
         const arrayElements = JSON.parse(elements)
-  
-
         const newArray  = arrayElements.map((val,ind)=>{
-            
             let where = {}
             where.news_id = news.id
-            
             where.order = val.order
-          
             const file = file_element(where.order)
           
             if(val.order) where.order=val.order 
@@ -53,25 +38,82 @@ route
             
             if(keys.includes(file)){
                 const {imgPath} = createPathImg(req.files[file])
-                
                 where.imgPath = imgPath
             }
-           
            
             return where
         })
         
         
-        const promiseFiles =  newArray.map(async(val)=>{
+        const promiseFiles =  newArray.map((val)=>{
+            if(!val.imgPath)return 
             const files = req.files[file_element(val.order)]
             savesImg(files,val.imgPath)
         })
+        const mainImg = savesImg(req.files['file-main'],imgPath)
         await Promise.all([ 
+            mainImg,
             Elements.bulkCreate( newArray ),
             promiseFiles
-            , savesImg(req.files['file-main'],imgPath)
          ])
         res.status(201).send({message:'Sucess'})
+    }catch(err){
+        res.status(500).send({message:'Something went wrong'+err})
+    }
+})
+.delete('/news/destroy',async(req,res)=>{
+    const {user_id}= req.user
+    const {news_id} = req.body
+    let deleteImgNews;
+    try {
+        const findNews = await findOne({where:{creator:user_id,id:news_id}})
+        if(!findNews)return res.status( 500 ).send({message:'error'})
+        
+        const findElements = await Elements.findAll({where:{news_id}})
+        const deleteFiles = findElements.val((val)=>{
+            if(val.imgPath && existImg(val.imgPath))fs.unlink(val.imgPath)
+            
+        })
+        const existImgNews = existImg(findNews.imgPath)
+       
+        await Promise.all([deleteFiles,fs.unlink(findNews.imgPath)])
+        res.status(201).send({message:'Sucess'})
+    } catch (error) {
+        res.status(500).send({message:'Something went wrong'+err})
+    }
+})
+.delete('/news/elements/destroy',async(req,res)=>{
+    const {user_id} = req.user
+    const {elements_id} = req.body
+    const ids = [...elements_id]
+
+    try{
+    const datas= await Elements.findAll({
+        include:[{
+            model:News,
+            as:'news',
+            required:true    
+        }],
+        where:{id:ids,'$news.creator$':user_id}
+    })
+
+    const imgs=datas.map((val)=>{
+        if(val.imgPath && existImg(val.imgPath))fs.unlink(val.imgPath)
+        
+    })
+    const deleteElements =  Elements.findAll({
+        include:[{
+            model:News,
+            as:'news',
+            required:true    
+        }],
+        where:{id:ids,'$news.creator$':user_id}
+    })
+
+    await Promise.all([
+        deleteElements, imgs
+    ])
+    res.status(201).send({message:'Sucess'})
     }catch(err){
         res.status(500).send({message:'Something went wrong'+err})
     }
